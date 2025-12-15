5d993c920c511dc2f705124e10678911c9aed1aa
Emmanuel Regaudie <emmanuelregaudie5@gmail.com>
2025-12-15 17:52:09 +0100

    Conf5

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,16 @@
 // vite.config.js
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+import { resolve } from "path";
+
 export default defineConfig({
   plugins: [tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        projet1: resolve(__dirname, "pages/projet1.html"),
+      },
+    },
+  },
 });
